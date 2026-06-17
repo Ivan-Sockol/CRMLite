778cd65e971ff21a50b3d4cd22ccac44c3891437
Sokol <sockolencko.iwan@yandex.ru>
2026-06-17 17:18:22 +0300

    add_custom_user_manager

--- crmlite/crmApp/models.py
+++ crmlite/crmApp/models.py
@@ -1,5 +1,36 @@
+from django.contrib.admindocs.utils import explicit_title_re
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+# Кастомный менеджер для регистрации пользователя
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email должен быть указан')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def crete_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -13,6 +44,8 @@ class User(AbstractUser):
                                 on_delete=models.SET_NULL,
                                 blank=True,
                                 related_name='users')
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
