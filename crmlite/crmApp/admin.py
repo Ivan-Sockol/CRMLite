d6e4ddeb8e49c52c20085a1320446d6c36bd9667
Sokol <sockolencko.iwan@yandex.ru>
2026-06-18 19:39:37 +0300

    Update models admin

--- crmlite/crmApp/admin.py
+++ crmlite/crmApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Company, Storage
+from .models import User, Company, Storage, Supplier, Supply, Product, SupplyProduct
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -15,3 +15,29 @@ class CompanyAdmin(admin.ModelAdmin):
 class StorageAdmin(admin.ModelAdmin):
     list_display = ('address', 'company')
     list_filter = ('company', )
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'inn', 'company')
+    search_fields = ('name', 'inn')
+    list_filter = ('company',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'purchase_price', 'sale_price', 'company')
+    list_filter = ('company',)
+    search_fields = ('name',)
+
+@admin.register(SupplyProduct)
+class SupplyProductInline(admin.TabularInline):
+    model = SupplyProduct
+    extra = 1
+    raw_id_fields = ('product',)
+
+@admin.register(Supply)
+class SupplyAdmin(admin.ModelAdmin):
+    list_display = ('id','supplier', 'company', 'delivery_date')
+    list_filter = ('company', 'supplier', 'delivery_date')
+    readonly_fields = ('delivery_date',)
+    inlines = (SupplyProductInline,)
+
--- crmlite/crmApp/models.py
+++ crmlite/crmApp/models.py
@@ -1,4 +1,3 @@
-from django.contrib.admindocs.utils import explicit_title_re
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -75,3 +74,81 @@ class Storage(models.Model):
 
     def __str__(self):
         return self.address
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=250)
+    inn = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey('Company',
+                                on_delete=models.CASCADE,
+                                related_name='suppliers',
+                                verbose_name='Компания')
+    
+    class Meta:
+        verbose_name='Поставщик'
+        verbose_name_plural='Поставщики'
+        unique_together=('company', 'inn')
+
+    def __str__(self):
+        return f'{self.name} ИНН:{self.inn}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    company = models.ForeignKey('Company',
+                                on_delete=models.CASCADE,
+                                related_name='products',
+                                verbose_name='Компания')
+
+    class Meta:
+        verbose_name='Товар'
+        verbose_name_plural='Товары'
+
+    def __str__(self):
+        return f'{self.name} - {self.quantity}'
+
+    @property
+    def profit_per_unit(self):
+        return self.sale_price - self.purchase_price
+
+class Supply(models.Model):
+    supplier = models.ForeignKey('Supplier',
+                                 on_delete=models.PROTECT,
+                                 related_name='supplies',
+                                 verbose_name='Поставщик')
+    company = models.ForeignKey('Company',
+                                on_delete=models.CASCADE,
+                                related_name='supplies',
+                                verbose_name='Компания')
+    delivery_date = models.DateTimeField(auto_now_add=True)
+    # Связь ManyToMany через промежуточную таблицу
+    products = models.ManyToManyField('Product',
+                                      through='SupplyProduct',
+                                      related_name='supplies',
+                                      verbose_name='Товары в поставке')
+
+    class Meta:
+        verbose_name='Поставка'
+        verbose_name_plural='Поставки'
+
+    def __str__(self):
+        return f'Поставка #{self.id}, от {self.delivery_date.strftime('%d.%m.%Y %H:%M')}'
+
+class SupplyProduct(models.Model):
+    supply = models.ForeignKey('Supply',
+                               on_delete=models.CASCADE)
+    product = models.ForeignKey('Product',
+                                on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    purchase_price_at_supply = models.DecimalField(max_digits=10,
+                                                   decimal_places=2)
+
+    class Meta:
+        verbose_name='Товар в поставке'
+        verbose_name_plural='Товары в поставке'
+        unique_together=('supply', 'product')
+
+    def __str__(self):
+        return f'{self.product.name} в поставке #{self.product.id}'
